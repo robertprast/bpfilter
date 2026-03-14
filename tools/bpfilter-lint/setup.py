3b515dd8e311106cdd18366c5a611484c05dd2fb
robertprast <robertprast@users.noreply.github.com>
2026-03-14 04:14:39 -0400

    fix: bump version, force fresh install

--- tools/bpfilter-lint/setup.py
+++ tools/bpfilter-lint/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 setup(
     name="bpfilter-lint",
-    version="0.1.1",
+    version="0.1.4",
     description="Linting utilities for bpfilter PRs",
     packages=["bpfilter_lint"],
     entry_points={"console_scripts": ["bpfilter-lint=bpfilter_lint.cli:main"]},
